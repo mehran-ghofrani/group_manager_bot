038628bb902f906ee2d7b036e4ce6ec8adb323e1
mehran ghofrani <mehran.ghofrani.74@gmail.com>
2018-10-01 16:01:54 +0330

    secret changed

--- src/main/java/test1/Secrets.java
+++ src/main/java/test1/Secrets.java
@@ -2,6 +2,6 @@ package test1;
 
 public class Secrets {
     public static String getToken() {
-        return "673597454:AAHu3gjMuIKHtUwa1cf_OMq8CYm6jdy7UVE";
+        return "673597454:AAFfzGASvahXvH3NwRZ2mSwFViC_LUlf9xY";
     }
 }
